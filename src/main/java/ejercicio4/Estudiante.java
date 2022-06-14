4969ee768c894725697dfcec101467a777b0df1e
JMateoVelascoAuza <mateovelascop19@gmail.com>
2022-06-14 05:01:41 -0700

    primer commit

---
+++ src/main/java/ejercicio4/Estudiante.java
@@ -0,0 +1,16 @@
+package ejercicio4;
+
+import javax.naming.Name;
+
+public class Estudiante {
+    private String Nombre;
+    private String ci;
+
+    public Estudiante() {}
+    public String getNombre() {
+        return Nombre;
+    }
+     public String getNombre {
+    return Name;
+    }
+}
